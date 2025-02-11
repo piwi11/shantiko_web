c45db09bdd2f55cbb4289ea32d035f4b1da7062b
piwi2012 <zupemartin@gmail.com>
2025-02-11 20:36:22 +0100

    add scripts folder with script.css, add the Lato font from google fonts and a slide menu with the menu button from google icons

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+}
+
+function closeMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.remove('active');
+}
