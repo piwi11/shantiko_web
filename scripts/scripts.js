158feab4d46eeffcc7e474298b431c67b04ab8b9
Artur Mirabet <arturmirabet@gmail.com>
2025-04-01 23:23:26 +0200

    feat(carousel): implement glide slider

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,20 @@
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-    toggleMenuButton()
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+    toggleMenuButton();
 }
 
 function closeMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.remove('active');
+    const menu = document.getElementById("menu");
+    menu.classList.remove("active");
 }
 
 function toggleMenuButton() {
-    const menu_button = document.getElementById('menu_button');
-    menu_button.classList.toggle('active');
+    const menu_button = document.getElementById("menu_button");
+    menu_button.classList.toggle("active");
 }
+
+const polaroids = document.querySelectorAll(".polaroid");
+Array.from(polaroids).forEach(function (element) {
+    element.addEventListener("click", () => console.log(this));
+});
