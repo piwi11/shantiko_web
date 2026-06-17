aab705dacb45b5f2e9c4a8523b6a31462ff1541f
Artur Mirabet <arturmirabet@gmail.com>
2026-06-17 19:54:13 +0200

    wip: stite relative urls

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,7 @@
-const baseUrl = (process.env.SITE_URL || "https://shantiko.com").replace(/\/+$/, "");
+const baseUrl = (process.env.SITE_URL || "https://shantiko.com").replace(
+    /\/+$/,
+    ""
+);
 
 export default {
     url: baseUrl
